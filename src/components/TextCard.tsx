e5ff9b4343140c0dac28aaa2de2f40a0995a19ee
Erick Ruiz <erickruiz29@gmail.com>
2021-09-22 21:20:52 -0700

    remove unused interface

--- src/components/TextCard.tsx
+++ src/components/TextCard.tsx
@@ -3,8 +3,6 @@ import {PropsWithChildren} from "react";
 
 import "./TextCard.scss"
 
-interface TextCard extends CardProps {}
-
 export function TextCard(props: PropsWithChildren<CardProps>) {
     return (
         <Card {...{...props, className: props.className ?? "" + " textCard"}}>
