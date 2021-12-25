bb7ca24b352aa0295b137bec5464514fd83333c3
Erick Ruiz <erickruiz29@gmail.com>
2021-12-25 15:31:00 -0800

    working

--- src/components/layout/NavigationMenu.tsx
+++ src/components/layout/NavigationMenu.tsx
@@ -1,17 +1,74 @@
-import { NavLink } from 'react-router-dom';
-
 import styles from './NavigationMenu.module.scss';
 
+const links = [
+  {
+    title: 'ErickRuiz.net',
+    id: 'content01',
+  },
+  {
+    title: 'About',
+    id: 'content02',
+  },
+  {
+    title: 'Volume.com',
+    id: 'content03',
+  },
+  {
+    title: 'UCLA',
+    id: 'content04',
+  },
+  {
+    title: 'California',
+    id: 'content05',
+  },
+  {
+    title: 'Faith',
+    id: 'content06',
+  },
+  {
+    title: 'Contact',
+    id: 'content07',
+  },
+  {
+    title: 'Hire me',
+    id: 'content08',
+  },
+];
+
+const clearActive = () => {
+  const links = document.querySelectorAll(`.${styles.navLinks} a`);
+
+  links.forEach((link) => {
+    link.classList.remove(styles.active)
+  })
+}
+
 export function NavigationMenu() {
+  let menu: HTMLElement
   return (
-    <div className={styles.navigationMenu}>
+     <div ref={(instance: HTMLDivElement) => {menu = instance}} className={styles.navigationMenu}>
       <div className={styles.navLinks}>
-        <NavLink to={'/'} title="ErickRuiz.net" exact>
-          ErickRuiz.net
-        </NavLink>
-        <NavLink to={'/about-me'} title="About Me">
-          About Me
-        </NavLink>
+        {links.map((link, i) => {
+          return <a
+          key={link.title}
+          aria-current="page"
+          title="About"
+          href={'/'}
+          className={i === 0 ? styles.active : ""}
+          onClick={(e) => {
+            e.preventDefault();
+            const el = document.querySelector(`#${link.id}`)
+            const clickedItem = e.currentTarget;
+            if (el !== null) {
+              window.scrollTo({top: el.getBoundingClientRect().y + window.scrollY - menu.getBoundingClientRect().height, behavior: "smooth"});
+              clearActive();
+              clickedItem.classList.add(styles.active)
+            }
+          }}
+        >
+          {link.title}
+        </a>
+        })}
       </div>
     </div>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ export function HomePage() {
   return (
     <Layout>
       <div className={styles.parallaxContainer}>
-        <div className={[styles.parallax, styles.content01].join(" ")}>
+        <div id="content01" className={[styles.parallax, styles.content01].join(" ")}>
             <Content>{`Some More text
             
             Some more text
@@ -16,14 +16,14 @@ export function HomePage() {
             
             all more text`}</Content>
         </div>
-        <div className={[styles.parallax, styles.content02].join(" ")} />
-        <div className={[styles.parallax, styles.content03].join(" ")} />
-        <div className={[styles.parallax, styles.content04].join(" ")} />
-        <div className={[styles.parallax, styles.content05].join(" ")} />
-        <div className={[styles.parallax, styles.content06].join(" ")} />
-        <div className={[styles.parallax, styles.content07].join(" ")} />
-        <div className={[styles.parallax, styles.content08].join(" ")} />
-        <div className={[styles.parallax, styles.content09].join(" ")} />
+        <div id="content02" className={[styles.parallax, styles.content02].join(" ")} />
+        <div id="content03" className={[styles.parallax, styles.content03].join(" ")} />
+        <div id="content04" className={[styles.parallax, styles.content04].join(" ")} />
+        <div id="content05" className={[styles.parallax, styles.content05].join(" ")} />
+        <div id="content06" className={[styles.parallax, styles.content06].join(" ")} />
+        <div id="content07" className={[styles.parallax, styles.content07].join(" ")} />
+        <div id="content08" className={[styles.parallax, styles.content08].join(" ")} />
+        <div id="content09" className={[styles.parallax, styles.content09].join(" ")} />
         </div>
     </Layout>
   );
