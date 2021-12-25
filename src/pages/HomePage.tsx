0133a44aa7898873d0f61a1aeb77f8c0d2a3817e
Erick Ruiz <erickruiz29@gmail.com>
2021-12-24 19:15:52 -0800

    fixes still need more

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Layout } from '../components/layout/Layout';
 
+import styles from './HomePage.module.scss'
+
 export function HomePage() {
   return (
     <Layout>
-      <div className="parallax-container">
-        <div className="parallax content-01" />
-        <div className="parallax content-02" />
-        <div className="parallax content-03" />
-        <div className="parallax content-04" />
-        <div className="parallax content-05" />
-        <div className="parallax content-06" />
-        <div className="parallax content-07" />
-        <div className="parallax content-08" />
-        <div className="parallax content-09" />
-      </div>
+      <div className={styles.parallaxContainer}>
+        <div className={[styles.parallax, styles.content01].join(" ")} />
+        <div className={[styles.parallax, styles.content02].join(" ")} />
+        <div className={[styles.parallax, styles.content03].join(" ")} />
+        <div className={[styles.parallax, styles.content04].join(" ")} />
+        <div className={[styles.parallax, styles.content05].join(" ")} />
+        <div className={[styles.parallax, styles.content06].join(" ")} />
+        <div className={[styles.parallax, styles.content07].join(" ")} />
+        <div className={[styles.parallax, styles.content08].join(" ")} />
+        <div className={[styles.parallax, styles.content09].join(" ")} />
+        </div>
     </Layout>
   );
 }
