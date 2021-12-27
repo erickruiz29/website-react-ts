799fa7f7d7dc0bafc09d2bba0107cb8589ff5708
Erick Ruiz <erickruiz29@gmail.com>
2021-12-26 20:13:30 -0800

    wip

---
+++ src/components/ui/Frame.tsx
@@ -0,0 +1,29 @@
+import { MouseEventHandler } from 'react';
+import { IPropsWithChildren } from '../../utils/util';
+import styles from './Frame.module.scss';
+
+export interface IFrameProps extends IPropsWithChildren {
+  description: string;
+  imgSrc: string;
+  randRotation?: boolean;
+  multiplier?: number;
+  onClick?: MouseEventHandler<HTMLDivElement>
+}
+
+export function Frame(props: IFrameProps) {
+  const negPos = Math.random() < 0.5 ? -1 : 1;
+  const randNum =
+    (Math.random() * (props.multiplier ? props.multiplier : 113) * negPos) % 6;
+  return (
+    <div
+      className={styles.frame}
+      style={{ transform: props.randRotation ? `rotate(${randNum}deg)` : '' }}
+      onClick={props.onClick}
+    >
+      <div className={styles.photoContainer}>
+        <img src={props.imgSrc} alt={props.description} />
+      </div>
+      <div className={styles.footer}>{props.description}</div>
+    </div>
+  );
+}
--- src/pages/Scrapbook.tsx
+++ src/pages/Scrapbook.tsx
@@ -1,21 +1,55 @@
-import React, { Fragment, ReactElement } from 'react';
+import React, { Fragment, ReactElement, useState } from 'react';
 import { ContentGrid } from '../components/layout/ContentGrid';
 import { Layout } from '../components/layout/Layout';
+import { Frame } from '../components/ui/Frame';
 
-// import styles from './Scrapbook.module.scss';
+import styles from './Scrapbook.module.scss';
+import imageSrc from './../assets/images/01.jpg';
+import { FrameModal, IFrameModalProps } from '../components/ui/FrameModal';
 
 export function Scrapbook() {
-  let blocks: ReactElement[] = []
-  for(let i = 0; i < 24; i += 1) {
-    blocks.push(<div key={`gridItem_${i}`}>{`gridItem_${i + 1}`}</div>)
+  const [modalProps, setModalProps] = useState<IFrameModalProps | undefined>(
+    undefined
+  );
+  let hasTriggered = false
+  window.addEventListener('resize', () => {
+    if (!hasTriggered) {
+      hasTriggered = true;
+      if (modalProps !== undefined) {
+        setModalProps({ ...modalProps, docHeight: window.outerHeight });
+      }
+      window.setTimeout(() => {
+        hasTriggered = false;
+      }, 500);
+    }
+  });
+
+  let blocks: ReactElement[] = [];
+  for (let i = 1; i <= 24; i += 1) {
+    blocks.push(
+      <Frame
+        key={i}
+        multiplier={i * 100}
+        imgSrc={imageSrc}
+        description={`grid item ${i}`}
+        randRotation={false}
+        onClick={() => {
+          setModalProps({
+            imgSrc: imageSrc,
+            description: `grid item ${i}`,
+            docHeight: modalProps?.docHeight ?? window.outerHeight,
+            overlayClick: () => {
+              setModalProps(undefined);
+            },
+          });
+        }}
+      />
+    );
   }
   return (
-    <Layout navMenu={<Fragment/>} contentGridSizing={true}>
-      <ContentGrid>
-        {
-          blocks
-        }
-      </ContentGrid>
+    <Layout navMenu={<Fragment />} contentGridSizing={true}>
+      <ContentGrid classes={[styles.scrapbook]}>{blocks}</ContentGrid>
+      {modalProps && <FrameModal {...modalProps} />}
     </Layout>
   );
 }
